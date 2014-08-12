4de1c7b0190743ce1e8aaaa4026f8312388534f6
Nischaal Cooray <nc475@drexel.edu>
2014-08-12 16:13:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 				
 				if (dataInf.type === 'dir') {
 					dName = (dataInf.name).replace('-', ' ');
-					$('#list').append('<span class = "item"><a href = "http://nischaalc.github.io/designProjects/'+dName+'/index.html">'+dName+'</a><a href = "'+dataInf.html_url+'">[GitHub]</a></span>');
+					$('#list').append('<span class = "item"><a href = "http://nischaalc.github.io/smallProjects/'+dName+'/index.html">'+dName+'</a><a href = "'+dataInf.html_url+'">[GitHub]</a></span>');
 				}
 			});
 		}
